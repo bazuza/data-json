efb6184774d8c7d1670cd05489f18cf99b3fc1b5
bazuza <bazuzello@gmail.com>
2019-06-04 12:55:31 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,41 @@
+var data = [
+  {
+    term_name: "Schools & Colleges",
+    term_id: 16,
+    post_description:
+      "All donations collected will be distributed to Open Door Ministries of High Point. Reserve your complimentary ticket and be sure to bring a non-perishable food item to donate at the door to help our local community. ",
+    post_title: "Spring Dance Concert",
+    post_id: 112
+  },
+  {
+    term_name: "Schools & Colleges",
+    term_id: 16,
+    post_description:
+      "Join us on March 21st at 4:00pm in the Sechrest Art Gallery for our New Impressions Gallery Talk and Reception. This Exhibit iThe Sechrest Art Gallery is open 9 a.m. to noon and 1 to 5 p.m. Tuesday through Friday.",
+    post_title: "New Impressions Gallery Talk and Reception",
+    post_id: 110
+  },
+  {
+    term_name: "Majors - Minors",
+    term_id: 15,
+    post_description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu maximus turpis.Lorem ipsum dolor sit amet, consectetur adipis",
+    post_title: "Lorem ipsum",
+    post_id: 99
+  },
+  {
+    term_name: "Majors - Minors",
+    term_id: 15,
+    post_description: "Graphic Design Today’s Featured Major",
+    post_title: "Lorem ipsum dolor",
+    post_id: 100
+  },
+  {
+    term_name: "Housing facilities",
+    term_id: 37,
+    post_description:
+      "“I’m constantly reminding myself of what Steve Jobs once said, ‘Being the richest man in the cemetery doesn’t matter to me. Going to bed at night saying we’ve done something wonderful, that’s what matters to me.’",
+    post_title: "Lorem 1",
+    post_id: 545
+  }
+];
